9ad080a6028c0c037ef43b313f786104a5a4976e
luckyboy2790 <rorifloris78@gmail.com>
2025-03-11 04:24:27 +0000

    fix: remove download button from schedule page

--- src/views/schedule/ScheduleList/components/ScheduleListActionTools.tsx
+++ src/views/schedule/ScheduleList/components/ScheduleListActionTools.tsx
@@ -1,28 +1,12 @@
 import Button from '@/components/ui/Button'
-import { TbCloudDownload, TbUserPlus } from 'react-icons/tb'
+import { TbUserPlus } from 'react-icons/tb'
 import { useNavigate } from 'react-router-dom'
-import useEmployeeList from '../hooks/useScheduleList'
-import { CSVLink } from 'react-csv'
 
 const ScheduleListActionTools = () => {
     const navigate = useNavigate()
 
-    const { scheduleList } = useEmployeeList()
-
     return (
         <div className="flex flex-col md:flex-row gap-3">
-            <CSVLink
-                className="w-full"
-                filename="scheduleList.csv"
-                data={scheduleList}
-            >
-                <Button
-                    icon={<TbCloudDownload className="text-xl" />}
-                    className="w-full"
-                >
-                    Download
-                </Button>
-            </CSVLink>
             <Button
                 variant="solid"
                 icon={<TbUserPlus className="text-xl" />}
