77951d4dbf68150203962bc54f56cdb71ab1ad10
luckyboy2790 <rorifloris78@gmail.com>
2025-03-20 02:47:31 +0000

    fix: add switch button to user profile dropdown

--- src/components/template/UserProfileDropdown.tsx
+++ src/components/template/UserProfileDropdown.tsx
@@ -4,8 +4,9 @@ import withHeaderItem from '@/utils/hoc/withHeaderItem'
 import { useSessionUser } from '@/store/authStore'
 import { Link } from 'react-router-dom'
 import { PiUserDuotone, PiGearDuotone, PiSignOutDuotone } from 'react-icons/pi'
+import { FaUserLock } from 'react-icons/fa'
 import { useAuth } from '@/auth'
-import type { JSX } from 'react'
+import { useEffect, useState, type JSX } from 'react'
 const domain = import.meta.env.VITE_BACKEND_ENDPOINT
 
 type DropdownList = {
@@ -25,6 +26,8 @@ const dropdownItemList: DropdownList[] = [
 const _UserDropdown = () => {
     const { img, full_name, email } = useSessionUser((state) => state.user)
 
+    const [isPersonal, setIsPersonal] = useState<boolean>(false)
+
     const { signOut } = useAuth()
 
     const handleSignOut = () => {
@@ -35,22 +38,40 @@ const _UserDropdown = () => {
         ...(img ? { src: `${domain}${img}` } : { icon: <PiUserDuotone /> }),
     }
 
+    const { user } = useAuth()
+
+    const isPersonalRoute = (): boolean => {
+        const pathSegments = window.location.pathname.split('/').filter(Boolean)
+
+        return pathSegments.length > 0 && pathSegments[0] === 'personal'
+    }
+
+    useEffect(() => {
+        console.log(isPersonalRoute())
+
+        if (isPersonalRoute()) {
+            setIsPersonal(true)
+        } else {
+            setIsPersonal(false)
+        }
+    }, [])
+
     return (
         <Dropdown
             className="flex"
             toggleClassName="flex items-center"
             renderTitle={
-                <div className="cursor-pointer flex items-center">
+                <div className="flex cursor-pointer items-center">
                     <Avatar size={32} {...avatarProps} />
                 </div>
             }
             placement="bottom-end"
         >
             <Dropdown.Item variant="header">
-                <div className="py-2 px-3 flex items-center gap-3">
+                <div className="flex gap-3 items-center px-3 py-2">
                     <Avatar {...avatarProps} />
                     <div>
-                        <div className="font-bold text-gray-900 dark:text-gray-100">
+                        <div className="text-gray-900 dark:text-gray-100 font-bold">
                             {full_name || 'Anonymous'}
                         </div>
                         <div className="text-xs">
@@ -67,13 +88,32 @@ const _UserDropdown = () => {
                     className="px-0"
                 >
                     <Link className="flex h-full w-full px-2" to={item.path}>
-                        <span className="flex gap-2 items-center w-full">
+                        <span className="flex w-full gap-2 items-center">
                             <span className="text-xl">{item.icon}</span>
                             <span>{item.label}</span>
                         </span>
                     </Link>
                 </Dropdown.Item>
             ))}
+            {user.account_type === 'Admin' && !isPersonal && (
+                <Dropdown.Item
+                    key="Switch to My Account"
+                    eventKey="Switch to My Account"
+                    className="px-0"
+                >
+                    <a
+                        className="flex h-full w-full px-2"
+                        href="/personal/dashboard"
+                    >
+                        <span className="flex w-full gap-2 items-center">
+                            <span className="text-xl">
+                                <FaUserLock />
+                            </span>
+                            <span>Switch to My Account</span>
+                        </span>
+                    </a>
+                </Dropdown.Item>
+            )}
             <Dropdown.Item variant="divider" />
             <Dropdown.Item
                 eventKey="Sign Out"
