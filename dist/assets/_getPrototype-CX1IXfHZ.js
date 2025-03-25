39d05e00aef85c471702e6515ff7e97f9809dbdd
luckyboy2790 <rorifloris78@gmail.com>
2025-03-25 03:03:28 +0000

    fix: update vercel.json file

--- dist/assets/_getPrototype-CX1IXfHZ.js
+++
@@ -1,1 +0,0 @@
-import{ai as a}from"./index-D0yRYkRK.js";var r,e;function i(){if(e)return r;e=1;var t=a(),o=t(Object.getPrototypeOf,Object);return r=o,r}export{i as r};
--- dist/assets/classNames-CVFWYafE.js
+++
@@ -1,1 +0,0 @@
-import{af as a,y as e}from"./index-D0yRYkRK.js";function t(...s){return a(e(s))}export{t as c};
