0c996a79c97875961da38e6d63e2238d4c26f7b1
luckyboy2790 <rorifloris78@gmail.com>
2025-03-25 01:48:17 +0000

    feat: complete personal dashboard page

--- src/views/personalDashboard/components/DashboardContent.tsx
+++ src/views/personalDashboard/components/DashboardContent.tsx
@@ -1,49 +1,130 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '@/components/ui/Card'
 import { FaRegUserCircle, FaRegClock, FaHome } from 'react-icons/fa'
 import EmployeeList from './EmployeeList'
+import { useToken } from '@/store/authStore'
+
+const domain = import.meta.env.VITE_BACKEND_ENDPOINT
 
 interface tabData {
     id: number
     tabTitle: string
     label1: string
-    value1: number
+    value1: number | string
     label2: string
-    value2: number
+    value2: number | string
     icon: React.ReactNode
 }
 
-const tabList: tabData[] = [
-    {
-        id: 1,
-        tabTitle: 'Attendance (Current Month)',
-        label1: 'Late Arrivals',
-        value1: 7,
-        label2: 'Early Departures',
-        value2: 2,
-        icon: <FaRegClock />,
-    },
-    {
-        id: 2,
-        tabTitle: 'Present Schedule',
-        label1: 'Time',
-        value1: 0,
-        label2: 'Rest Days	',
-        value2: 21,
-        icon: <FaRegUserCircle />,
-    },
-    {
-        id: 3,
-        tabTitle: 'leaves of absence',
-        label1: 'Approved',
-        value1: 0,
-        label2: 'Pending',
-        value2: 0,
-        icon: <FaHome />,
-    },
-]
-
 const DashboardContent = () => {
+    const [tabList, setTabList] = useState<tabData[]>([
+        {
+            id: 1,
+            tabTitle: 'Attendance (Current Month)',
+            label1: 'Late Arrivals',
+            value1: 0,
+            label2: 'Early Departures',
+            value2: 0,
+            icon: <FaRegClock />,
+        },
+        {
+            id: 2,
+            tabTitle: 'Present Schedule',
+            label1: 'Time',
+            value1: '',
+            label2: 'Rest Days',
+            value2: '',
+            icon: <FaRegUserCircle />,
+        },
+        {
+            id: 3,
+            tabTitle: 'leaves of absence',
+            label1: 'Approved',
+            value1: 0,
+            label2: 'Pending',
+            value2: 0,
+            icon: <FaHome />,
+        },
+    ])
+
+    const { token } = useToken()
+
+    const changeTimeFormat = (time: string): string => {
+        const currentDate = new Date()
+
+        const currentYear = currentDate.getFullYear()
+
+        const date = new Date(`${currentYear}-01-01T${time}`)
+
+        if (isNaN(date.getTime())) {
+            return 'Invalid Time'
+        }
+
+        let hours = date.getUTCHours()
+        let minutes = date.getUTCMinutes()
+
+        const period = hours >= 12 ? 'PM' : 'AM'
+
+        hours = hours % 12
+        hours = hours ? hours : 12
+
+        const minutesString = minutes < 10 ? `0${minutes}` : `${minutes}`
+
+        return `${hours}:${minutesString} ${period}`
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const repsonse = await fetch(
+                `${domain}/api/dashboard/personal/get_card_data`,
+                {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        ...(token ? { Authorization: `Bearer ${token}` } : {}),
+                    },
+                },
+            )
+
+            const result = await repsonse.json()
+
+            console.log(result)
+
+            setTabList([
+                {
+                    id: 1,
+                    tabTitle: 'Attendance (Current Month)',
+                    label1: 'Late Arrivals',
+                    value1: result.lateArrivals,
+                    label2: 'Early Departures',
+                    value2: result.earlyDepartures,
+                    icon: <FaRegClock />,
+                },
+                {
+                    id: 2,
+                    tabTitle: 'Present Schedule',
+                    label1: 'Time',
+                    value1: `${changeTimeFormat(result.recentSchedule.start_time)} - ${changeTimeFormat(result.recentSchedule.off_time)}`,
+                    label2: 'Rest Days	',
+                    value2: result.recentSchedule.rest_days.map(
+                        (item: string) => <span>{`${item}, `}</span>,
+                    ),
+                    icon: <FaRegUserCircle />,
+                },
+                {
+                    id: 3,
+                    tabTitle: 'leaves of absence',
+                    label1: 'Approved',
+                    value1: result.approvedLeave,
+                    label2: 'Pending',
+                    value2: result.pendingLeave,
+                    icon: <FaHome />,
+                },
+            ])
+        }
+
+        fetchData()
+    }, [])
     return (
         <div>
             <div className="mt-8">
