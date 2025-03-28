f5a708951a46d2b9e0ee58624bcd1dc206b3308f
luckyboy2790 <rorifloris78@gmail.com>
2025-03-28 16:04:44 +0000

    feat: import and export company function

--- src/views/companies/components/CompanyActionTools.tsx
+++ src/views/companies/components/CompanyActionTools.tsx
@@ -1,17 +1,67 @@
 import Button from '@/components/ui/Button'
 import { TbCloudDownload, TbCloudUpload } from 'react-icons/tb'
 import { CSVLink } from 'react-csv'
-import useCustomerList from '../hooks/useCompanyList'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import { Company, CompanyCreateResponse } from '../types'
+import { apiCreateCompany, apiTotalCompanies } from '@/services/CompanyService'
+import Papa from 'papaparse'
+import useCompanyList from '../hooks/useCompanyList'
+
+type ResponseType = {
+    list: Company[]
+    message: string
+    total: number
+}
+
+type CompanyCreateData = {
+    companyName: string
+}
 
 const CompanyActionTools = () => {
-    const companyList = useCustomerList()
+    const { mutate } = useCompanyList()
+
+    const [companyList, setCompanyList] = useState<Company[]>([])
+
+    const handleExport = async () => {
+        const employee: ResponseType = await apiTotalCompanies()
+
+        setCompanyList(employee.list)
+    }
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0]
+        if (file) {
+            Papa.parse(file, {
+                complete: async (result: any) => {
+                    if (result.data?.length > 0) {
+                        for (let company of result.data) {
+                            if (company._id || company._id !== '') {
+                                apiCreateCompany<
+                                    CompanyCreateResponse,
+                                    CompanyCreateData
+                                >({
+                                    companyName: company?.company_name,
+                                })
+                            }
+                        }
+                    }
+                    mutate()
+                },
+                header: true,
+            })
+        }
+    }
+
+    useEffect(() => {
+        handleExport()
+    }, [])
 
     return (
         <div className="flex flex-col md:flex-row gap-3">
             <CSVLink
                 className="w-full"
                 filename="companyList.csv"
-                data={companyList.companyList}
+                data={companyList}
             >
                 <Button
                     icon={<TbCloudDownload className="text-xl" />}
@@ -20,12 +70,23 @@ const CompanyActionTools = () => {
                     Download
                 </Button>
             </CSVLink>
-            <Button
-                variant="solid"
-                icon={<TbCloudUpload className="text-xl" />}
-            >
-                Import
-            </Button>
+            <div className="w-full">
+                <Button
+                    variant="solid"
+                    icon={<TbCloudUpload className="text-xl" />}
+                >
+                    <label htmlFor="file-upload" className="cursor-pointer">
+                        Import CSV
+                    </label>
+                </Button>
+                <input
+                    type="file"
+                    id="file-upload"
+                    accept=".csv"
+                    className="hidden"
+                    onChange={handleFileChange}
+                />
+            </div>
         </div>
     )
 }
