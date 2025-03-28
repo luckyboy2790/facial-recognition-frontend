9034c51f30c8c383d35ef51c90dab96c86492006
luckyboy2790 <rorifloris78@gmail.com>
2025-03-28 18:53:36 +0000

    feat: complete export and import job title data

--- src/views/jobTitles/components/JobsActionTools.tsx
+++ src/views/jobTitles/components/JobsActionTools.tsx
@@ -2,18 +2,70 @@ import Button from '@/components/ui/Button'
 import { TbCloudDownload, TbCloudUpload } from 'react-icons/tb'
 import { useNavigate } from 'react-router-dom'
 import { CSVLink } from 'react-csv'
+import { JobTitle, JobTitleCreateResponse } from '../types'
+import useJobTitleList from '../hooks/useJobsList'
+import { useEffect, useState } from 'react'
+import { apiGetAllData } from '@/services/employeeService'
+import Papa from 'papaparse'
+import { apiCreateJobTitle } from '@/services/JobTitleService'
+
+type JobTitleCreateData = {
+    departmentId: string | null
+    jobTitle: string
+    company: string | null
+}
+
+type responseType = {
+    jobTitle: JobTitle[]
+}
 
 const CompanyActionTools = () => {
-    const navigate = useNavigate()
+    const { mutateJobTitles } = useJobTitleList()
 
-    const customerList: any = []
+    const [jobTitleList, setJobTitleList] = useState<JobTitle[]>([])
+
+    const handleExport = async () => {
+        const data: responseType = await apiGetAllData()
+
+        setJobTitleList(data.jobTitle)
+    }
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0]
+        if (file) {
+            Papa.parse(file, {
+                complete: async (result: any) => {
+                    if (result.data?.length > 0) {
+                        for (let jobTitle of result.data) {
+                            if (jobTitle._id || jobTitle._id !== '') {
+                                apiCreateJobTitle<
+                                    JobTitleCreateResponse,
+                                    JobTitleCreateData
+                                >({
+                                    company: jobTitle?.company,
+                                    departmentId: jobTitle?.department_id,
+                                    jobTitle: jobTitle?.job_title,
+                                })
+                            }
+                        }
+                    }
+                    mutateJobTitles()
+                },
+                header: true,
+            })
+        }
+    }
+
+    useEffect(() => {
+        handleExport()
+    }, [])
 
     return (
         <div className="flex flex-col md:flex-row gap-3">
             <CSVLink
                 className="w-full"
-                filename="customerList.csv"
-                data={customerList}
+                filename="jobTitleList.csv"
+                data={jobTitleList}
             >
                 <Button
                     icon={<TbCloudDownload className="text-xl" />}
@@ -22,12 +74,23 @@ const CompanyActionTools = () => {
                     Download
                 </Button>
             </CSVLink>
-            <Button
-                variant="solid"
-                icon={<TbCloudUpload className="text-xl" />}
-            >
-                Import
-            </Button>
+            <div className="w-full">
+                <Button
+                    variant="solid"
+                    icon={<TbCloudUpload className="text-xl" />}
+                >
+                    <label htmlFor="file-upload" className="cursor-pointer">
+                        Import CSV
+                    </label>
+                </Button>
+                <input
+                    type="file"
+                    id="file-upload"
+                    accept=".csv"
+                    className="hidden"
+                    onChange={handleFileChange}
+                />
+            </div>
         </div>
     )
 }
