ff80413c68cb08c6d82958fe4a18faf01b57ef5f
luckyboy2790 <rorifloris78@gmail.com>
2025-07-03 19:48:47 +0100

    update: update English locale assets

--- src/components/template/LanguageSelector.tsx
+++ src/components/template/LanguageSelector.tsx
@@ -8,8 +8,8 @@ import { HiCheck } from 'react-icons/hi'
 import type { CommonProps } from '@/@types/common'
 
 const languageList = [
-    { label: 'English', value: 'en', flag: 'US' },
     { label: 'Espanol', value: 'es', flag: 'ES' },
+    { label: 'English', value: 'en', flag: 'US' },
 ]
 
 const _LanguageSelector = ({ className }: CommonProps) => {
